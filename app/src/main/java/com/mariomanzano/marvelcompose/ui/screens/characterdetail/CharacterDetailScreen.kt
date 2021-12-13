eae5ee1751d545ca9be0ad73211d0e041fbec603
Mario Manzano <mario.manzano@experis.es>
2021-12-13 21:45:58 +0100

    Added character find repository call to CharacterDetailScreen.kt

--- app/src/main/java/com/mariomanzano/marvelcompose/ui/screens/characterdetail/CharacterDetailScreen.kt
+++ app/src/main/java/com/mariomanzano/marvelcompose/ui/screens/characterdetail/CharacterDetailScreen.kt
@@ -3,15 +3,29 @@ package com.mariomanzano.marvelcompose.ui.screens.characterdetail
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.mariomanzano.marvelcompose.data.model.Character
+import com.mariomanzano.marvelcompose.data.repositories.CharactersRepository
+
 
 @Composable
 fun CharacterDetailScreen(id: Int) {
+    var characterState by remember { mutableStateOf<Character?>(null) }
+    
+    LaunchedEffect(Unit) {
+        characterState = CharactersRepository.find(id)
+    }
+
+    characterState?.let { CharacterDetailScreen(it) }
+
+}
+@Composable
+fun CharacterDetailScreen(character: Character) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()){
-        Text(text = id.toString())
+            Text(text = character.title)
     }
 }
